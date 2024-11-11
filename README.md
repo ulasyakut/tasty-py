0de55e3324761ac4f697b40bf9f291b91e3a1ba5
Kristofer <kris@youngers.org>
2024-11-11 09:23:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# tasty
+a simple task manager
+
+## What to do?
+
+This project is a very simple terminal based, personal task manager.
+
+Your goal is to write the python code in the class to make it work.
+
+Once finished, I'll give you a solution written by someone else, and you'll compare an docntrast the way you did it with another.
+
+## How does it work?
+TASTY is very easy to use. Here's the full list of commands, which you can also find directly by using it.
+
+
+## Two problems
+
+- How to represent a task within the class
+- How to save/load tasks
+
+_Look at the last two lines of Tasty.py, what is happening there?_
+
+### Phase 1 Get these commands working...
+
+`help`: display a help message
+
+`exit`: exit Tasty
+
+`clear`: clear the screen
+
+
+### Phase 2 and now these
+
+`new <task name>`: create a new task
+
+`tasks`: display all the tasks
+
+### Phase 3 make task persistant and change state
+
+`save`: save your tasks, the trash and your advancement
+
+`load`: load the save file (saved_tasks.json)
+
+`complete <task name>`: complete a task
+
+`unfinish <task name>`: mark a task as unfinished
+
+### Phase 4 finish with these
+
+`remove <task name>`: move a task in the trash
+
+`destroy <task name>`: remove a task from the trash (removed definetely)
+
+`trash`: display the content of the trash
+
+`advancements`: display the amount of completed and unfinished tasks
+
---
+++ Tasty.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env  python3
+
+import sys
+import json
+import os
+
+class Tasty:
+    """
+    Tasty class.
+    :param tasks: the user tasks
+    :param important_tasks: the important user tasks
+    :param complete: the completed tasks
+    :param unfinished: the unfinished tasks
+    :param trash: the user trash
+    :param version: the Tasty version
+    :param save_file: the Tasty save file
+    """
+
+    # make sure to do an __init__ method
+
+    def __init__(self):
+        pass
+
+    def help(self):
+        """
+        Display a help message.
+        """
+        print("Tasty Help ")
+        print("============================================================================")
+        print("help                     ->        display this message")
+        print("tasks                    ->        display all your tasks")
+        print("trash                    ->        display the content of the trash")
+        print("new <task>               ->        add a new task")
+        print("remove <task>            ->        add a task to the trash")
+        print("complete <task>          ->        complete a task")
+        print("unfinish <task>          ->        unfinish a task")
+        print("recover <task>           ->        recover a removed task")
+        print("destroy <task>           ->        remove a task from the trash")
+        print("advancement              ->        see the tasks advancement")
+        print("exit                     ->        exit Tasty")
+        print("save                     ->        save your current tasks")
+        print("load                     ->        load a save file")
+        print("clear                    ->        clear the screen")
+        
+    def license(self):
+        """
+        Display the MIT License terms for Tasty.
+        """
+        print("""
+Copyright (c) 2024 Tasty
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        """)
+
+
+if __name__ == "__main__":
+    tasty = Tasty()
+    tasty.help()
+    while True:
+        command = input("Tasty> ")
+        if command == "exit":
+            pass # exit the program, how would you do this?
+        elif command == "help":
+            tasty.help()
+        elif command == "license":
+            tasty.license()
+        else:
+            print("Unknown command")
